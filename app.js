4523e1b317ccb43d57696344902910482e55b124
rocio <rocio.gabriela.03c@gmail.com>
2021-12-15 10:22:12 +0100

    changes

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      products: [],
-      categories: [],
-    }
-  },
-  mounted() {
-    this.loadProducts()
-    this.loadCategories()
-  },
-  methods: {
-    async loadProducts() {
-      let response = await fetch('https://fakestoreapi.com/products')
-      this.products = await response.json()
-    },
-    async loadCategories() {
-      let response = await fetch('https://fakestoreapi.com/products/categories')
-      this.categories = await response.json()
-    },
-  },
-})
-app.mount('#vue-app')
+	data() {
+		return {
+			products: [],
+			categories: [],
+			selectedCategory: "lanadelrey",
+		};
+	},
+	mounted() {
+		this.loadProducts();
+		this.loadCategories();
+	},
+	methods: {
+		async loadProducts() {
+			let response = await fetch("https://fakestoreapi.com/products");
+			this.products = await response.json();
+		},
+		async loadCategories() {
+			let response = await fetch(
+				"https://fakestoreapi.com/products/categories"
+			);
+			this.categories = await response.json();
+		},
+		filterButtonClicked(category) {
+			this.selectedCategory = category;
+		},
+	},
+});
+app.mount("#vue-app");
